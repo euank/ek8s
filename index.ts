4ee06c2bf109c6f7a2ebb2dd1176c2f710a8a763
Euan Kemp <euank@euank.com>
2021-04-20 23:03:59 -0700

    Drop demo deployment

--- index.ts
+++ index.ts
@@ -4,16 +4,3 @@ import * as kx from "@pulumi/kubernetesx";
 new k8s.yaml.ConfigFile("cert-manager", {
     file: "./deps/cert-manager/cert-manager.yaml",
 });
-
-const appLabels = { app: "nginx" };
-const deployment = new k8s.apps.v1.Deployment("nginx", {
-    spec: {
-        selector: { matchLabels: appLabels },
-        replicas: 1,
-        template: {
-            metadata: { labels: appLabels },
-            spec: { containers: [{ name: "nginx", image: "nginx" }] }
-        }
-    }
-});
-export const name = deployment.metadata.name;
